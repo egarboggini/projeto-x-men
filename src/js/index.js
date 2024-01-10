494fef093ef9dae28675da4c6c08c34f07dd6dc9
Emmanuel <154647800+egarboggini@users.noreply.github.com>
2024-01-10 00:38:23 -0300

    update js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-const personagens = document.querySelectorAll('personagem');
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        personagem.classList.add('selecionado');
+    } )
+})
