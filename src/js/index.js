91c80b3818f61955f2f1c14fca162be9ba120711
Emmanuel <154647800+egarboggini@users.noreply.github.com>
2024-01-12 00:10:10 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,12 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
+        
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+        
         personagem.classList.add('selecionado');
 
-        const personagemSelecionado =
+       
     } )
 })
