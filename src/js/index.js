eee6c0c55fa1878f7b2a33de5a7ad007e77fcb27
Emmanuel <154647800+egarboggini@users.noreply.github.com>
2024-01-12 00:39:51 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,15 @@ personagens.forEach((personagem) => {
         
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado')
-        
         personagem.classList.add('selecionado');
 
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/images/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
        
+        const descricaoPersonagem = document.get
     } )
 })
